30d8415367521848dc933590f6d88095b4439d84
raphael costa <raphael.costa21@hotmail.com>
2024-11-26 10:10:15 -0300

    feat: add logger and audit logs to controller file

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -16,11 +16,13 @@ export const cadastrarUsuario = async (req: Request, res: Response) => {
             let novoUsuario = await Usuario.create({ email, senha, nome, disciplina });
 
             res.status(201);
+            logger.info(`Uusário cadastrado com o ip: ${req.socket.remoteAddress} e o email: ${novoUsuario.email}`);
             return res.json({
                 message: "Usuário cadastrado com sucesso.",
                 novoUsuario
             });
         } else {
+            logger.error(`Cadastro negado por e-mail já utilizado. e-mail utilizado: ${email}`);
             return res.status(400).json({ error: 'E-mail já existe.' });
         }
     }
@@ -38,9 +40,11 @@ export const fazerLogin = async (req: Request, res: Response) => {
         });
 
         if (usuario) {
+            logger.info(`Login feio através do e-mail: ${email}`);
             res.json({ status: true });
             return;
         }
+        logger.error("Login recusado por falta de credenciais, ou credenciais erradas");
     }
 
     res.json({ status: false });
@@ -75,11 +79,14 @@ export const deletarUsuario = async (req: Request, res: Response) => {
             user.destroy();
             await user.save(); // Salva as alterações do status
 
+            logger.info(`Usuário ${deletedUserName} foi removido com sucesso.`);
             res.status(200).json({ message: `Usuário ${deletedUserName} foi removido com sucesso.`, status: '200' });
         } else {
+            logger.error(`Usuário com ID ${id} não encontrado. Falha ao tentar removê-lo.`);
             res.status(404).json({ message: `Usuário com ID ${id} não encontrado.`, status: '404' });
         }
     } catch (error) {
+        // logger.error("Erro na tentativa de remover o usuário. \n", error);
         res.status(500).json({ message: 'Ocorreu um erro ao remover o usuário.', status: '500', error });
     }
 }
@@ -92,6 +99,7 @@ export const atualizarUsuario = async (req: Request, res: Response) => {
     const estaVazio = Object.values(valores).some((valor) => valor === null || valor === '');
 
     if (estaVazio) {
+        logger.error("Erro ao tentar atualizar usuário, por falta de dados ou dados incompletos.");
         return res.status(400).json({ mensagem: 'Os dados enviados estão incompletos.', status: '400' });
     }
 
@@ -99,6 +107,7 @@ export const atualizarUsuario = async (req: Request, res: Response) => {
         const usuarioEncontrado = await Usuario.findOne({ where: { id } });
 
         if (!usuarioEncontrado) {
+            logger.error("Erro ao tentar atualizar usuário, o usuário não foi encontrado.");
             return res.status(404).json({ mensagem: 'Usuário não encontrado.', status: '404' });
         }
 
@@ -106,10 +115,11 @@ export const atualizarUsuario = async (req: Request, res: Response) => {
 
         const usuarioAtualizado = await Usuario.findOne({ where: { id } });
 
+        logger.info(`O usuário com o e-mail ${usuarioAtualizado?.email}, foi atualizado com sucesso!`);
         return res.status(200).json({ mensagem: 'Usuário atualizado com sucesso.', status: '200', usuarioAtualizado });
     } catch (erro) {
         console.error(erro);
-
+        logger.error("Ocorreu um erro ao tentar atualizar o usuário.");
         return res.status(500).json({ mensagem: 'Erro ao atualizar usuário.', status: '500', erro });
     }
 }
